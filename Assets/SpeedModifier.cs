21f89318c9fc79cca82725b6ceec5e1cc2218f8d
sneakthot <47699879+sneakthot@users.noreply.github.com>
2021-01-08 23:43:07 -0500

    boosted speed of enemy needs refinement

---
+++ Assets/SpeedModifier.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using EmeraldAI;
+using UnityEngine;
+
+public class SpeedModifier : MonoBehaviour
+{
+
+    public EmeraldAISystem sys;
+
+    void Start()
+    {
+        sys.WalkSpeed *= 3;
+        sys.RunSpeed *= 3;
+    }
+
+    
+}
