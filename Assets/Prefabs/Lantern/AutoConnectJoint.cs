64149e627cd8214bb2d87ebcacac50ac333102c9
sneakthot <47699879+sneakthot@users.noreply.github.com>
2021-01-14 22:11:53 -0500

    added lanterns

---
+++ Assets/Prefabs/Lantern/AutoConnectJoint.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class AutoConnectJoint : MonoBehaviour
+{
+    
+    void Awake()
+    {
+        GetComponent<CharacterJoint>().connectedBody = transform.parent.GetComponent<Rigidbody>();
+    }
+}
+
---
+++ Assets/Scripts/WindBlower.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindBlower : MonoBehaviour
+{
+    public int Xdir = 0;
+    public int Ydir = 0;
+    public int Zdir = 0;
+
+    private GameObject[] windObjects;
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        windObjects = GameObject.FindGameObjectsWithTag("windable");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (GameObject obj in windObjects)
+        {
+            Rigidbody[] rbs = obj.GetComponentsInChildren<Rigidbody>();
+            foreach (Rigidbody rb in rbs)
+            {
+                rb.AddForce(new Vector3(Xdir + Random.Range(-1, 1), Ydir + Random.Range(-1, 1), Zdir + 
+                    Random.Range(-1, 1)));
+            }
+        }
+    }
+}
