8af7ad639e256096efac056895d3c90f63946c27
Rohan Agarwal <ro.agarwal@hotmail.com>
2021-03-05 18:57:36 -0500

    Add form change effect

---
+++ Assets/Scenes/Rohan/Assets_Rohan/DissolveContent/ShaderLerp.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShaderLerp : MonoBehaviour {
+
+	float handler = 0f;
+	public bool grow = false;
+	public bool dissolve = false;
+
+	
+	public float secondDuration = 3f;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (grow)
+		{	
+			float fragmentation = 1	 / secondDuration;
+			handler += fragmentation * Time.deltaTime;
+			
+			if (handler > 1)
+				grow = false;
+		}
+
+		if (dissolve)
+		{
+			float fragmentation = -1 / secondDuration;
+			handler += fragmentation * Time.deltaTime;
+			
+			if (handler < 0)
+				dissolve = false;
+		}
+
+        gameObject.GetComponent<SkinnedMeshRenderer>().material.SetFloat("_DissolveAmount", handler);
+		// Shader.SetGlobalFloat ("_Dissolve", handler);
+	}
+
+	public void Grow ()
+	{
+		handler = 1;
+		dissolve = true;
+		grow = false;
+	}
+
+	public void Dissolve ()
+	{
+		handler = 0;
+		dissolve = false;
+		grow = true;
+	}
+}
