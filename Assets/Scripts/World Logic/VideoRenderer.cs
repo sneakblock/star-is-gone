d61a25f30e9f0a8ef01aa7a08a31ae94519e2b4a
Rohan Agarwal <ro.agarwal@hotmail.com>
2021-03-16 21:27:59 -0400

    Add TV video playing

---
+++ Assets/Scripts/World Logic/VideoRenderer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoRenderer : MonoBehaviour
+{
+    public List<VideoClip> clips = new List<VideoClip>();
+    public int currChannel = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.GetComponent<VideoPlayer>().clip = clips[currChannel];
+        Debug.Log(clips[currChannel]);
+        Debug.Log(gameObject.GetComponent<VideoPlayer>().clip);
+    }
+
+    public void changeChannel(int channelNum) {
+        currChannel = Mathf.Clamp(channelNum, 0, clips.Count);
+    }
+}
