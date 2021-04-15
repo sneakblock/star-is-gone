c70750c2e506a0215edcee2aa205248abbff0352
block3 <block3@gatech.edu>
2021-04-15 16:28:13 -0400

    after fucking up nightclub brightness, before trying fix

---
+++ Assets/FlickerLights.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Antlr4.Runtime.Misc;
+using UnityEngine;
+using Aura2API;
+
+public class FlickerLights : MonoBehaviour
+{
+    
+    private ArrayList<Light> lights = new ArrayList<Light>();
+
+    void Start()
+    {
+        foreach (Light light in GameObject.Find("Lights").GetComponentsInChildren<Light>())
+        {
+            lights.Add(light);
+        }
+        InvokeRepeating(nameof(Flicker), 0, .1f);
+    }
+
+    private void Flicker()
+    {
+        foreach (Light light in lights)
+        {
+            var chance = Random.Range(0, 100);
+            light.intensity = chance >= 50 ? 5 : 0;
+        }
+    }
+}
