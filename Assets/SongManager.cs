fed474dd6843451658a521ad0a2d8d85782282b1
block3 <block3@gatech.edu>
2021-04-20 03:01:56 -0400

    vgdevbuild

---
+++ Assets/SongManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SongManager : MonoBehaviour
+{
+
+    public AudioSource from;
+
+    public AudioSource to;
+
+    private Collider _coll;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _coll = GetComponent<Collider>();
+    }
+
+    private void OnTriggerEnter(Collider _coll)
+    {
+        from.Pause();
+        to.Play();
+    }
+}
